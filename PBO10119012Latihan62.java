2ce9458fd2ba076195626eeb3079dd1da1161e16
damaisaputralaoli <73173498+damaisaputralaoli@users.noreply.github.com>
2020-11-29 12:14:58 +0700

    Add files via upload

---
+++ PBO10119012Latihan62.java
@@ -0,0 +1,20 @@
+
+package PBO10119012Latihan62
+
+/**
+ *   NAMA  : DAMAI SAPUTRA LAOLI
+ *   KELAS : IF-1
+ *   NIM   : 10119012
+ * @author damai
+ */
+public class PBO10119012Latihan62 {
+
+    
+    public static void main(String[] args) {
+        Human human = new Human();
+        human.setNama("Damai Saputra Laoli");
+        human.walk(human.getNama());
+        human.breath(human.getNama());
+        human.eat(human.getNama());
+    }
+}
